e1555a2fe6bafb22e398108f1fa37e2fa9549190
wanee-polytech <2060340007@office.kopo.ac.kr>
2020-08-19 23:10:36 +0900

    aop-xml 코드 수정

---
+++ xml/Action.java
@@ -0,0 +1,11 @@
+package aop.xml;
+
+public class Action {
+	public void gotoOffice() {
+		System.out.println("출근 중...");
+	}
+	
+	public void getoffOffice() {
+		System.out.println("퇴근 중...");
+	}
+}
---
+++ xml/Programmer.java
@@ -0,0 +1,11 @@
+package aop.xml;
+
+public class Programmer implements Employee{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		System.out.println("개발 중...");
+	}
+
+}
